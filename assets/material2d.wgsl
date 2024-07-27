a4ccb858f100b07d61aab57a09b9fd85909c222c
DGriffin91 <github@dgdigital.net>
2024-07-27 12:14:20 -0700

    render in shader

--- assets/material2d.wgsl
+++ assets/material2d.wgsl
@@ -4,6 +4,39 @@
 const TAU: f32 = 6.28318530717958647692528676655900577;
 const PI: f32 = 3.14159265358979323846264338327950288;
 
+fn uhash(a: u32, b: u32) -> u32 { 
+    var x = ((a * 1597334673u) ^ (b * 3812015801u));
+    // from https://nullprogram.com/blog/2018/07/31/
+    x = x ^ (x >> 16u);
+    x = x * 0x7feb352du;
+    x = x ^ (x >> 15u);
+    x = x * 0x846ca68bu;
+    x = x ^ (x >> 16u);
+    return x;
+}
+
+fn unormf(n: u32) -> f32 { 
+    return f32(n) * (1.0 / f32(0xffffffffu)); 
+}
+
+fn hash_noise(x: u32, y: u32, z: u32) -> f32 {
+    let urnd = uhash(x, (y << 11) + z);
+    return unormf(urnd);
+}
+
+fn get_arc_size(ring: u32, level: u32, seed: u32) -> f32 {
+    return (select(0.9,0.3,ring % 2==0)) / (f32(ring + 1));
+}
+
+fn get_ring_speed(ring: u32, level: u32, seed: u32) -> f32 {
+    return ((hash_noise(ring, level, seed) * 0.2 + 0.1) / (f32(ring + 1)))
+        * (1.0 + f32(ring) * 0.4);
+}
+
+fn get_ring_color(ring: u32, level: u32, seed: u32) -> u32 {
+    return u32(floor(hash_noise(ring + 2048, level, seed) * 1.0 - 0.0000001));
+}
+
 struct State {
     position: vec4<f32>,
     resolution: vec4<f32>,
@@ -11,6 +44,10 @@ struct State {
     ring_thick: f32,
     frame: f32,
     time: f32,
+    local_player_pos: u32,
+    player_offset: f32,
+    t: f32,
+    spare3: u32,
 }
 
 @group(2) @binding(0) var<uniform> state: State;
@@ -29,30 +66,62 @@ fn vertex(@builtin(vertex_index) vertex_index: u32) -> FullscreenVertexOutput {
     return FullscreenVertexOutput(clip_position, uv);
 }
 
+fn render(coord: vec2<f32>) -> vec3<f32> {
+    var color = vec3(0.0);
+    let pos = state.position.xy;
+    let uv = coord / state.resolution.xy;
+    let screen_mid = state.resolution.xy / 2.0;
+    let p = coord - screen_mid - pos * state.scale_factor;
+    let fring = length(p) / (state.ring_thick * state.scale_factor) + 1.0;
+    let ring = u32(floor(fring));
+    let theta = (atan2(p.y, p.x) + PI * 0.5) / TAU;
+
+    {
+        // Draw rings
+        let m = ring % 2;
+        if m == 0 {
+            color = vec3(0.2,0.6,0.2);
+        }
+        if m == 1 {
+            color = vec3(0.1,0.1,0.5);
+        }
+    }
+
+
+    {
+        // Draw arcs
+        let ring_speed = get_ring_speed(ring, 0u, 0u);
+        var arc_size: f32;
+        if ring == state.local_player_pos {
+            arc_size = 0.13 / (1.0 + f32(ring) * 0.03);
+        } else {
+            arc_size = get_arc_size(ring, 0u, 0u);
+        }
+
+        var ring_start = state.player_offset;
+        if ring != state.local_player_pos {
+            ring_start = fract(state.t * (ring_speed * f32(ring + 1)));
+        }
+
+        let start = fract(theta - ring_start);
+        if start < arc_size {
+            color = vec3(0.1, 0.02, 0.2);
+        }
+    }
+
+    return color;
+}
+
 @fragment
 fn fragment(input: FullscreenVertexOutput) -> @location(0) vec4<f32> {
-    let pos = state.position.xy;
-    let uv = input.position.xy / state.resolution.xy;
-    let screen_mid = state.resolution.xy / 2.0;
-    let p = input.position.xy - screen_mid - pos * state.scale_factor;
-    let ring = length(p) / (state.ring_thick * state.scale_factor);
-    let theta = (atan2(p.y, p.x) + PI) / TAU;
-
-    let m = ring % 2.0;
+    let coord = input.position.xy;
     var color = vec3(0.0);
-    if floor(m) == 0.0 {
-        color = vec3(0.2,0.6,0.2);
-    }
-    if floor(m) == 1.0 {
-        color = vec3(0.1,0.1,0.5);
-    }
-
-
-    //color = vec3(theta);
-    let r = fract(theta + state.time * 0.1);
-    if r > 0.25 && r < 0.5 && floor(ring) == 5.0 {
-        color = vec3(0.5, 0.1, 0.05);
-    }
-
+    color += render(coord);
+    // MSAA RGSS
+    // color += render(coord + vec2( 0.125,  0.375));
+    // color += render(coord + vec2(-0.375,  0.125));
+    // color += render(coord + vec2( 0.375, -0.125));
+    // color += render(coord + vec2(-0.125, -0.375));
+    // color *= 0.25;
     return vec4(color, 1.0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::f32::consts::*;
 
 use bevy::asset::AssetMetaCheck;
 use bevy::core_pipeline::fxaa::{Fxaa, Sensitivity};
-use bevy::core_pipeline::smaa::SmaaSettings;
+
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::math::*;
 use bevy::prelude::*;
@@ -18,12 +18,10 @@ use bevy_vector_shapes::{painter::ShapePainter, shapes::Cap};
 pub mod palette;
 pub mod sampling;
 
-use palette::RGB_PALETTE;
 use ridiculous_bevy_hot_reloading::{hot_reloading_macros::make_hot, HotReloadPlugin};
 use sampling::hash_noise;
 
-/// #[no_mangle] Needed so libloading can find this entry point
-#[no_mangle]
+#[no_mangle] // Needed so libloading can find this entry point
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::srgb(0.05, 0.05, 0.05)))
@@ -35,8 +33,6 @@ fn main() {
             DefaultPlugins
                 .set(AssetPlugin {
                     // Wasm builds will check for meta files (that don't exist) if this isn't set.
-                    // This causes errors and even panics in web builds on itch.
-                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
                     meta_check: AssetMetaCheck::Never,
                     ..default()
                 })
@@ -113,6 +109,7 @@ fn draw(
     painter.thickness = 5.0;
     painter.cap = Cap::None;
     let t = time.elapsed_seconds() * game_speed;
+    gpu.state.t = t;
     let mut pressed_up = false;
     if keyboard_input.just_pressed(KeyCode::ArrowUp) || keyboard_input.just_pressed(KeyCode::KeyW) {
         pressed_up = true;
@@ -137,62 +134,58 @@ fn draw(
     }
 
     let local_player_pos = 10 + *player_ring;
+    gpu.state.local_player_pos = local_player_pos;
 
-    let ring_thick = (25.0 - (*player_ring as f32) * 0.25).max(8.0);
+    let ring_thick = (25.0 - (*player_ring as f32) * 0.25).max(5.0);
     gpu.state.ring_thick = ring_thick;
+    gpu.state.player_offset = *player_offset;
 
-    //{
-    //    let t_player = *player_offset * TAU;
-    //    let position =
-    //        vec3(-t_player.sin(), -t_player.cos(), 0.0) * local_player_pos as f32 * ring_thick;
-    //    //dbg!(position);
-    //    gpu.state.position = vec4(position.x, -position.y, 0.0, 0.0);
-    //
-    //    painter.set_translation(position);
-    //}
+    {
+        let t_player = *player_offset * TAU;
+        let position =
+            vec3(-t_player.sin(), -t_player.cos(), 0.0) * local_player_pos as f32 * ring_thick;
 
-    //for i in 0..115u32 + local_player_pos {
-    //    let color = if i % 2 == 0 {
-    //        RGB_PALETTE[1][1]
-    //    } else {
-    //        RGB_PALETTE[1][2]
-    //    };
-    //    if i % 2 == 0 {
-    //        painter.set_color(color);
-    //        arc(&mut painter, 0.0, 1.0, i, ring_thick);
-    //    }
-    //}
+        gpu.state.position = vec4(position.x, -position.y, 0.0, 0.0);
 
-    if true {
-        return;
+        painter.set_translation(position);
+    }
+    let draw_debug_rings = false;
+    let draw_debug_arc = false;
+
+    if draw_debug_rings {
+        for i in 0..115u32 + local_player_pos {
+            if i % 2 == 0 {
+                painter.set_color(Color::srgb(0.0, 1.0, 1.0));
+                arc(&mut painter, 0.0, 1.0, i, ring_thick);
+            }
+        }
     }
 
     let mut missed = false;
-    for i in local_player_pos..60u32 + local_player_pos {
-        let game_ring = i;
-
-        let ring_speed = get_ring_speed(game_ring, 0, 0, i);
-        let arc_size = if i == local_player_pos {
-            0.13 / (1.0 + i as f32 * 0.03)
+    let start = local_player_pos;
+    let mut end = local_player_pos + 2;
+    if draw_debug_arc {
+        end = local_player_pos + 60; // Only eval further out rings if draw debug
+    }
+    for ring in start..end {
+        let ring_speed = get_ring_speed(ring, 0, 0);
+        let arc_size = if ring == local_player_pos {
+            0.13 / (1.0 + ring as f32 * 0.03)
         } else {
-            get_arc_size(game_ring + 1024, 0, 0, i)
-        };
-        let mut ring_color_idx = get_ring_color(game_ring, 0, 0);
-
-        let p = if i == local_player_pos {
-            ring_color_idx = *player_color_idx;
-            *player_offset
-        } else {
-            (t * (ring_speed * (i + 1) as f32)).rem_euclid(1.0)
+            get_arc_size(ring, 0, 0)
         };
 
-        painter.set_color(idx_color(ring_color_idx));
-        if i == local_player_pos && pressed_up {
-            let next_speed = get_ring_speed(game_ring + 1, 0, 0, i + 1);
-            let mut this_p = p;
+        let mut ring_start = *player_offset;
+        if ring != local_player_pos {
+            ring_start = (t * (ring_speed * (ring + 1) as f32)).fract();
+        }
+
+        if ring == local_player_pos && pressed_up {
+            let next_speed = get_ring_speed(ring + 1, 0, 0);
+            let mut this_p = ring_start;
             let mut this_size = arc_size;
-            let mut next_size = get_arc_size(game_ring + 1 + 1024, 0, 0, i + 1);
-            let mut next_p = (t * (next_speed * (i + 2) as f32)).rem_euclid(1.0);
+            let mut next_size = get_arc_size(ring + 1, 0, 0);
+            let mut next_p = (t * (next_speed * (ring + 2) as f32)).rem_euclid(1.0);
             if arc_size > next_size {
                 (this_p, next_p) = (next_p, this_p);
                 (this_size, next_size) = (next_size, this_size);
@@ -213,30 +206,32 @@ fn draw(
             painter.circle(5000.0);
         }
 
-        arc(&mut painter, p, arc_size, i, ring_thick);
-
+        if draw_debug_arc {
+            painter.set_color(Color::srgb(1.0, 0.0, 1.0));
+            arc(&mut painter, ring_start, arc_size, ring, ring_thick);
+        }
         {
             // Edge Lines
-            if i == local_player_pos {
+            if ring == local_player_pos {
                 painter.thickness = 0.5;
                 painter.cap = Cap::None;
                 let inset = 0.0;
-                let p = p.fract() + inset;
+                let p = ring_start.fract() + inset;
                 painter.set_color(Color::WHITE);
                 painter.arc(
-                    ring_thick * ((i + 1) as f32) - ring_thick,
+                    ring_thick * ((ring + 1) as f32) - ring_thick,
                     TAU * p,
                     TAU * (p + arc_size - inset * 2.0),
                 );
             }
-            if i == local_player_pos + 1 {
+            if ring == local_player_pos + 1 {
                 painter.thickness = 0.5;
                 painter.cap = Cap::None;
                 let inset = 0.0;
-                let p = p.fract() + inset;
+                let p = ring_start.fract() + inset;
                 painter.set_color(Color::WHITE);
                 painter.arc(
-                    ring_thick * ((i + 1) as f32) - (ring_thick - 1.0) - ring_thick,
+                    ring_thick * ((ring + 1) as f32) - (ring_thick - 1.0) - ring_thick,
                     TAU * p,
                     TAU * (p + arc_size - inset * 2.0),
                 );
@@ -245,38 +240,25 @@ fn draw(
     }
 }
 
-fn arc(painter: &mut ShapePainter, t: f32, size: f32, ring: u32, ring_thick: f32) {
+fn arc(painter: &mut ShapePainter, start: f32, size: f32, ring: u32, ring_thick: f32) {
     painter.hollow = true;
-    painter.thickness = ring_thick;
+    let debug_thick_scale = 0.5;
+    painter.thickness = ring_thick * debug_thick_scale;
     painter.cap = Cap::None;
-    let t = t.fract();
+    let start = start.fract();
     painter.arc(
         ring_thick * ((ring + 1) as f32) - ring_thick,
-        TAU * t,
-        TAU * (t + size),
+        TAU * start,
+        TAU * (start + size),
     );
 }
 
-fn get_arc_size(ring: u32, level: u32, attempt: u32, local_ring: u32) -> f32 {
-    (if ring % 2 == 0 { 0.3 } else { 0.9 }) / ((local_ring + 1) as f32)
+fn get_arc_size(ring: u32, level: u32, seed: u32) -> f32 {
+    (if ring % 2 == 0 { 0.3 } else { 0.9 }) / ((ring + 1) as f32)
 }
 
-fn get_ring_speed(ring: u32, level: u32, attempt: u32, local_ring: u32) -> f32 {
-    ((hash_noise(ring, level, attempt) * 0.2 + 0.1) / ((local_ring + 1) as f32))
-        * (1.0 + ring as f32 * 0.4)
-}
-
-fn get_ring_color(ring: u32, level: u32, attempt: u32) -> u32 {
-    (hash_noise(ring + 2048, level, attempt) * 1.0 - 0.0000001).floor() as u32
-}
-
-fn idx_color(i: u32) -> Color {
-    match i {
-        0 => RGB_PALETTE[0][1],
-        1 => RGB_PALETTE[1][4],
-        2 => RGB_PALETTE[2][4],
-        _ => RGB_PALETTE[0][0],
-    }
+fn get_ring_speed(ring: u32, level: u32, seed: u32) -> f32 {
+    ((hash_noise(ring, level, seed) * 0.2 + 0.1) / ((ring + 1) as f32)) * (1.0 + ring as f32 * 0.4)
 }
 
 #[derive(Clone, ShaderType, Default, Debug)]
@@ -287,6 +269,10 @@ struct GpuState {
     ring_thick: f32,
     frame: f32,
     time: f32,
+    local_player_pos: u32,
+    player_offset: f32,
+    t: f32,
+    spare3: u32,
 }
 
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone, Default)]
